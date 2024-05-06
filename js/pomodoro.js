1de71829b0a653e154d82683f94dd083d979d63a
user <dnerizb2223@politecnics.barcelona>
2024-05-06 20:18:58 +0200

    mas o menos

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let isPomodoro = true;
     const pomodoroDuration = { minutes: 25, seconds: 0 };
     const shortBreakDuration = { minutes: 5, seconds: 0 };
-    const longBreakDuration = { minutes: 15, seconds: 0 };    
+    const longBreakDuration = { minutes: 15, seconds: 0 };
     let pomodoroCount = 1;
 
     function startTimer(duration, display) {
@@ -21,7 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (--totalSeconds < 0) {
                 clearInterval(timer);
-                // timerAlert();
                 if (isPomodoro) {
                     if (pomodoroCount < 4) {
                         startBreak(shortBreakDuration, "Descanso Corto");
@@ -31,10 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 } else {
                     pomodoroCount++;
                     if (pomodoroCount >= 5) {
-                        pomodoroCount = 0;
+                        pomodoroCount = 1;
                         document.getElementById("start").disabled = false;
+                        isRunning = false;
+                        document.getElementById("task-select").disabled = false;
+                    } else {
+                        startPomodoro(pomodoroDuration, "Pomodoro");
                     }
-                    startPomodoro(pomodoroDuration, "Pomodoro");
                 }
             }
         }, 1000);
@@ -43,20 +45,18 @@ document.addEventListener("DOMContentLoaded", () => {
     function startPomodoro(duration, label) {
         isPomodoro = true;
         document.getElementById("start").disabled = true;
-        document.getElementById("task-select").disabled = true; // Desactivar el select durante el Pomodoro
+        document.getElementById("task-select").disabled = true;
         timer = startTimer(duration, document.getElementById("timer"));
         document.getElementById("timer-label").textContent = label;
     }
-    
+
     function startBreak(duration, label) {
         isPomodoro = false;
         document.getElementById("start").disabled = true;
-        document.getElementById("task-select").disabled = true; // Activar el select durante la pausa corta
+        document.getElementById("task-select").disabled = true;
         timer = startTimer(duration, document.getElementById("timer"));
         document.getElementById("timer-label").textContent = label;
     }
-    
-    
 
     document.getElementById("start").addEventListener("click", () => {
         if (!isRunning) {
@@ -66,52 +66,19 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    // function timerAlert() {
-    //     alert("¡Tiempo completado!");
-    //     document.getElementById("task-select").disabled = false;
-    // }    
-
-    // Kanban
     const pendingColumn = document.getElementById("pending-column");
     const progressColumn = document.getElementById("progress-column");
     const completedColumn = document.getElementById("completed-column");
 
-    let draggedTask = null;
-
-    [pendingColumn, progressColumn, completedColumn].forEach(column => {
-        column.addEventListener("dragover", e => {
-            e.preventDefault();
-        });
-
-        column.addEventListener("drop", e => {
-            e.preventDefault();
-            const task = draggedTask;
-            const targetColumn = e.target.closest(".column");
-
-            if (targetColumn !== column) {
-                targetColumn.appendChild(task);
-                const newCategory = targetColumn.getAttribute("data-category");
-                task.dataset.category = newCategory;
-            }
-
-            draggedTask = null;
-        });
-    });
-
-    document.querySelectorAll(".task").forEach(task => {
-        task.addEventListener("dragstart", e => {
-            draggedTask = e.target;
-        });      
-    });
-
     document.addEventListener("click", event => {
-            if (event.target.classList.contains("task")) {
-                const confirmDelete = confirm("Are you sure you want to delete this task?");
-                if (confirmDelete) {
-                    event.target.remove();
-                }
+        if (event.target.classList.contains("task")) {
+            const confirmDelete = confirm("Are you sure you want to delete this task?");
+            if (confirmDelete) {
+                event.target.remove();
             }
-        });   
+        }
+    });
+
     const newTaskButton = document.getElementById("new-task-button");
     const newTaskModal = document.getElementById("new-task-modal");
 
@@ -131,17 +98,15 @@ document.addEventListener("DOMContentLoaded", () => {
         const description = document.getElementById("task-description").value;
         const category = document.getElementById("task-category").value;
         addTaskToPending(title, description, category);
-        updateTaskSelector(); // Actualizar el selector de tareas
+        updateTaskSelector();
         newTaskForm.reset();
         newTaskModal.style.display = "none";
     });
 
-    // Nueva función para actualizar el selector de tareas
     function updateTaskSelector() {
         const taskSelect = document.getElementById("task-select");
-        taskSelect.innerHTML = ""; // Limpiar opciones existentes
+        taskSelect.innerHTML = "";
 
-        // Recorrer todas las tareas y agregarlas como opciones
         document.querySelectorAll(".task").forEach(task => {
             const option = document.createElement("option");
             option.value = task.querySelector("h3").textContent;
@@ -151,9 +116,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function createTask(title, description, category) {
+        const taskId = 'task_' + Date.now(); // Genera un ID único
         const task = document.createElement("div");
         task.classList.add("task");
-        task.setAttribute("draggable", "true");
+        task.setAttribute("draggable", "true"); // Establece draggable en true para permitir el arrastre
+        task.id = taskId; // Asigna el ID único al elemento
         task.dataset.category = category;
 
         const taskTitle = document.createElement("h3");
@@ -172,13 +139,43 @@ document.addEventListener("DOMContentLoaded", () => {
         return task;
     }
 
+
     function addTaskToPending(title, description, category) {
         const task = createTask(title, description, category);
         pendingColumn.appendChild(task);
     }
 
-    // Inicialmente agregar una tarea de ejemplo
     addTaskToPending("Terminar Proyecto", "Completar las tareas restantes para el proyecto", "Backend");
-    // Actualizar el selector de tareas
     updateTaskSelector();
+
+    function allowDrop(ev) {
+        ev.preventDefault(); // Permite soltar elementos en el contenedor
+    }
+    
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id); // Guarda el ID del elemento arrastrado
+    }
+    
+    function drop(ev) {
+        ev.preventDefault(); // Evita el comportamiento predeterminado de la soltura
+        var data = ev.dataTransfer.getData("text"); // Obtiene el ID del elemento arrastrado
+        var draggedElement = document.getElementById(data); // Obtiene el elemento arrastrado del DOM
+    
+        // Verifica si el elemento arrastrado existe en el DOM
+        if (draggedElement) {
+            ev.target.appendChild(draggedElement); // Agrega el elemento arrastrado al contenedor de soltura
+        } else {
+            console.error("Elemento arrastrado no encontrado en el DOM");
+        }
+    }   
+
+    document.querySelectorAll('.task').forEach(task => {
+        task.addEventListener('dragstart', drag);
+    });
+
+    // Asignar eventos de soltura a las columnas con la clase 'column'
+    document.querySelectorAll('.column').forEach(column => {
+        column.addEventListener('dragover', allowDrop);
+        column.addEventListener('drop', drop);
+    });
 });
